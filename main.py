141ee0ceaa9022bc1b00f0d2581964e7f868fa97
cuhong <hongcoilhouse@gmail.com>
2023-05-28 01:04:37 +0900

    리드미 업데이트

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from allbaro import Allbaro
 if __name__ == '__main__':
 
     print(datetime.datetime.now())
-    ab = Allbaro()
-    ab.authenticate('cienergy3', '!ci69071001')
+    allbaro = Allbaro()
+    allbaro.authenticate('cienergy3', '!ci69071001')
     # ab.get_login_info()
     start_date = datetime.date(2023, 5, 1)
     end_date = datetime.date(2023, 5, 30)
-    result_list = ab.handover_process_list(start_date, end_date)
+    result_list = allbaro.handover_process_list(start_date, end_date)
+    for result in result_list:
+        print(result_list)
     with open('참고자료/result_list.csv', 'w') as file:
         wr = csv.DictWriter(file, fieldnames=result_list[0].keys())
         wr.writeheader()
